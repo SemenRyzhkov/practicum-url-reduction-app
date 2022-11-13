52520b8d566af3e0aedc49e1167f46c1de3a44a0
semen <thaisun2018@yandex.ru>
2022-11-13 18:13:06 +0300

    add .env file

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/SemenRyzhkov/practicum-url-reduction-app/internal/app"
 	"github.com/SemenRyzhkov/practicum-url-reduction-app/internal/config"
 )
 
+//BASE_URL=http://localhost:8080/;SERVER_ADDRESS=localhost:8081
+
 func main() {
-	os.Setenv("SERVER_ADDRESS", "localhost:8080")
-	os.Setenv("BASE_URL", "http://localhost:8080")
+	err := godotenv.Load(".env")
+	host := os.Getenv("BASE_URL")
+	fmt.Println("HOST " + host)
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	cfg, err := config.New()
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("SERVER " + cfg.Host)
 
 	a, err := app.New(cfg)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.0.7
+	github.com/joho/godotenv v1.4.0
 )
 
 require github.com/stretchr/objx v0.5.0 // indirect
