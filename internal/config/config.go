01a4949a6b6a1010fcdc0ba08631d55776b79c5e
semen <thaisun2018@yandex.ru>
2022-11-14 18:57:01 +0300

    add load .env file to test

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,22 +1,11 @@
 package config
 
 import (
-	"time"
-
 	"github.com/caarlos0/env/v6"
 )
 
-const (
-	//defaultHost         = "localhost:8080"
-	defaultWriteTimeout = 5 * time.Second
-	defaultReadTimeout  = 5 * time.Second
-)
-
 type Config struct {
-	Host         string        `env:"SERVER_ADDRESS"`
-	WriteTimeout time.Duration `env:"WRITE_TIMEOUT"`
-	ReadTimeout  time.Duration `env:"READ_TIMEOUT"`
-	//BaseURL      string        `env:"BASE_URL"`
+	Host string `env:"SERVER_ADDRESS"`
 }
 
 func New() (Config, error) {
