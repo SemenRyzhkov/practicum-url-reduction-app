951d79cdc1616a551f3b8e385c95208825b31c27
semen <thaisun2018@yandex.ru>
2022-11-19 13:50:43 +0300

    review refactoring part1

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,20 +1,33 @@
 package config
 
+//
+//type Config struct {
+//	Host string
+//}
+//
+//func New() (Config, error) {
+//	myEnv, err := godotenv.Read()
+//	fmt.Println(myEnv)
+//	if err != nil {
+//		return Config{}, err
+//	}
+//	return Config{Host: myEnv["SERVER_ADDRESS"]}, nil
+//}
 import (
-	"fmt"
-
-	"github.com/joho/godotenv"
+	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
-	Host string
+	Host string `env:"SERVER_ADDRESS"`
 }
 
 func New() (Config, error) {
-	myEnv, err := godotenv.Read()
-	fmt.Println(myEnv)
-	if err != nil {
-		return Config{}, err
+	cfg := Config{}
+	err := env.Parse(&cfg)
+	{
+		if err != nil {
+			return Config{}, err
+		}
 	}
-	return Config{Host: myEnv["SERVER_ADDRESS"]}, nil
+	return cfg, nil
 }
