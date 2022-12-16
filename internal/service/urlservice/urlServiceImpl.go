f98d709e6e8de9dd94a34cbdebd42f281488d8fd
semen <thaisun2018@yandex.ru>
2022-12-16 11:01:59 +0300

    add logger middleware

--- internal/service/urlservice/urlServiceImpl.go
+++ internal/service/urlservice/urlServiceImpl.go
@@ -94,7 +94,7 @@ func (u *urlServiceImpl) RemoveAll(ctx context.Context, userID string, removingL
 		fmt.Println(time.Since(now))
 	}()
 
-	outCh := make(chan entity.URLDTO)
+	outCh := make(chan entity.URLDTO, len(removingList))
 
 	go func() {
 		wg := &sync.WaitGroup{}
